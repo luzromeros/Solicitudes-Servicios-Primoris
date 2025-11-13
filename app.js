9c4b9256467c636b39f2d438325772c41bbbfec0
luzromeros <78057457+luzromeros@users.noreply.github.com>
2025-11-13 16:38:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ function loadImage(url) {
         }
         xhr.send();
     });}
-
 let signaturePad = null;
 
 window.addEventListener('load', async () => {
@@ -26,70 +25,47 @@ window.addEventListener('load', async () => {
         let NombreSolicitante = document.getElementById('NombreSolicitante').value;
         let Nit = document.getElementById('Nit').value;
         let Direccion = document.getElementById('Direccion').value;
-        let Cuidad = document.getElementById('Ciudad').value;
+        let Ciudad = document.getElementById('Ciudad').value;
         let PersonaContacto = document.getElementById('PersonaContacto').value;
         let TelefonoContacto = document.getElementById('TelefonoContacto').value;
         let NombreFacturante = document.getElementById('NombreFacturante').value;
         let NitFacturante = document.getElementById('NitFacturante').value;
-        let DireccionFacturante = document.getElementById('DireccionFacturante').value;
-        let CuidadFacturante = document.getElementById('CuidadFacturante').value;
+        let CiudadFacturante = document.getElementById('CiudadFacturante').value;
         let TelefonoFacturante = document.getElementById('TelefonoFacturante').value;
         var error= document.getElementById('error');
 
         if (document.getElementById('FacturanteSi').checked) {
             NombreFacturante = NombreSolicitante
             NitFacturante= Nit
-            DireccionFacturante = Direccion
-            CuidadFacturante= Cuidad
+            CiudadFacturante= Ciudad
             TelefonoFacturante= TelefonoContacto
 
         } else {
             NombreFacturante = NombreFacturante
             NitFacturante= NitFacturante 
-            DireccionFacturante= DireccionFacturante
-            CuidadFacturante= CuidadFacturante  
+            CiudadFacturante= CiudadFacturante  
             TelefonoFacturante= TelefonoFacturante                  
         }
 
         if (document.getElementById('FacturanteNo').checked) {
             NombreFacturante = NombreFacturante
             NitFacturante= NitFacturante 
-            DireccionFacturante= DireccionFacturante
-            CuidadFacturante= CuidadFacturante  
+            CiudadFacturante= CiudadFacturante  
             TelefonoFacturante= TelefonoFacturante
 
         } else {
             NombreFacturante = NombreSolicitante
             NitFacturante= Nit
-            DireccionFacturante = Direccion
-            CuidadFacturante= Cuidad
+            CiudadFacturante= Ciudad
             TelefonoFacturante =TelefonoContacto
         }
 
         let fact = document.getElementById('fact').value;
-
         let matriz = document.getElementById('matriz').value;
         let fecha= document.getElementById('fecha').value;
         let codigo= document.getElementById('codigo').value;
 
-  
-        
- 
         let multiresiduo = document.getElementById('multiresiduo').value;
-
-       
-        let MicoSi = document.getElementById('MicoSi').value;
-        let micotoxinaSeleccionada = "";
-            const radioSeleccionado = document.querySelector('input[name="micotoxinas"]:checked');
-            if (radioSeleccionado) {
-                micotoxinaSeleccionada = radioSeleccionado.value;
-                                    }  
-        let ditios = "";
-            const ditiosSeleccionado = document.querySelector('input[name="ditios"]:checked');
-            if (ditiosSeleccionado) {
-            ditios = ditiosSeleccionado.value;
-            }
-        
         let antimonio = document.getElementById('antimonio').value;
         let arsenico = document.getElementById('arsenico').value;
         let boro = document.getElementById('boro').value;
@@ -109,46 +85,148 @@ window.addEventListener('load', async () => {
         let potasio = document.getElementById('potasio').value;
         let metalesSi = document.getElementById('metalesSi');
 
-        
-        let glyfo = document.getElementById('glyfo').value;
-        let fosetyl = document.getElementById('fosetyl').value;
-        let clorato = document.getElementById('clorato').value;
-        let diquat = document.getElementById('diquat').value;
-        let oxEtil = document.getElementById('oxEtil').value;
-        let acrilamida = document.getElementById('acrilamida').value;
-        let amitraz= document.getElementById('amitraz').value;
-        let patulina= document.getElementById('patulina').value;
-        let gmo = document.getElementById('gmo').value;
-        let doscuatroD= document.getElementById('doscuatroD').value;
+        let acrilamida = document.getElementById('acrilamida').checked ? document.getElementById('acrilamida').value : '';
+        let AFL = document.getElementById('AFL').checked ? document.getElementById('AFL').value : '';
+        let AFLM = document.getElementById('AFLM').checked ? document.getElementById('AFLM').value : '';
+        let AFLOTA = document.getElementById('AFLOTA').checked ? document.getElementById('AFLOTA').value : '';
+        let amitraz = document.getElementById('amitraz').checked ? document.getElementById('amitraz').value : '';
+        let bromuro = document.getElementById('bromuro').checked ? document.getElementById('bromuro').value : '';
+        let ciromazina = document.getElementById('ciromazina').checked ? document.getElementById('ciromazina').value : '';
+        let clorato = document.getElementById('clorato').checked ? document.getElementById('clorato').value : '';
+        let cloratos = document.getElementById('cloratos').checked ? document.getElementById('cloratos').value : '';
+        let cloruro = document.getElementById('cloruro').checked ? document.getElementById('cloruro').value : '';
+        let cloro = document.getElementById('cloro').checked ? document.getElementById('cloro').value : '';
+        let DIOX_PCBDs = document.getElementById('DIOX_PCBDs').checked ? document.getElementById('DIOX_PCBDs').value : '';
+        let ditios = document.getElementById('ditios').checked ? document.getElementById('ditios').value : '';
+        let ditiosLow = document.getElementById('ditiosLow').checked ? document.getElementById('ditiosLow').value : '';
+        let DTCAN = document.getElementById('DTCAN').checked ? document.getElementById('DTCAN').value : '';
+        let DON = document.getElementById('DON').checked ? document.getElementById('DON').value : '';
+        let doscuatroD = document.getElementById('doscuatroD').checked ? document.getElementById('doscuatroD').value : '';
+        let ETH = document.getElementById('ETH').checked ? document.getElementById('ETH').value : '';
+        let ETHOX = document.getElementById('ETHOX').checked ? document.getElementById('ETHOX').value : '';
+        let FAL = document.getElementById('FAL').checked ? document.getElementById('FAL').value : '';
+        let FLON = document.getElementById('FLON').checked ? document.getElementById('FLON').value : '';
+        let FLUO = document.getElementById('FLUO').checked ? document.getElementById('FLUO').value : '';
+        let FOLP = document.getElementById('FOLP').checked ? document.getElementById('FOLP').value : '';
+        let fosetyl = document.getElementById('fosetyl').checked ? document.getElementById('fosetyl').value : '';
+        let FUM = document.getElementById('FUM').checked ? document.getElementById('FUM').value : '';
+        let GCCAN = document.getElementById('GCCAN').checked ? document.getElementById('GCCAN').value : '';
+        let GGA = document.getElementById('GGA').checked ? document.getElementById('GGA').value : '';
+        let GMOS = document.getElementById('GMOS').checked ? document.getElementById('GMOS').value : '';
+        let LCCAN = document.getElementById('LCCAN').checked ? document.getElementById('LCCAN').value : '';
+        let LHR = document.getElementById('LHR').checked ? document.getElementById('LHR').value : '';
+        let MC1 = document.getElementById('MC1').checked ? document.getElementById('MC1').value : '';
+        let MEL = document.getElementById('MEL').checked ? document.getElementById('MEL').value : '';
+        let MINO = document.getElementById('MINO').checked ? document.getElementById('MINO').value : '';
+        let NEG = document.getElementById('NEG').checked ? document.getElementById('NEG').value : '';
+        let NIT = document.getElementById('NIT').checked ? document.getElementById('NIT').value : '';
+        let NITRI = document.getElementById('NITRI').checked ? document.getElementById('NITRI').value : '';
+        let OTA = document.getElementById('OTA').checked ? document.getElementById('OTA').value : '';
+        let PAH = document.getElementById('PAH').checked ? document.getElementById('PAH').value : '';
+        let patulina = document.getElementById('patulina').checked ? document.getElementById('patulina').value : '';
+        let PER = document.getElementById('PER').checked ? document.getElementById('PER').value : '';
+        let PHOS = document.getElementById('PHOS').checked ? document.getElementById('PHOS').value : '';
+        let QU1 = document.getElementById('QU1').checked ? document.getElementById('QU1').value : '';
+        let QU2 = document.getElementById('QU2').checked ? document.getElementById('QU2').value : '';
+        let RAD1 = document.getElementById('RAD1').checked ? document.getElementById('RAD1').value : '';
+        let S421 = document.getElementById('S421').checked ? document.getElementById('S421').value : '';
+        let SULFA = document.getElementById('SULFA').checked ? document.getElementById('SULFA').value : '';
+        let SULFI = document.getElementById('SULFI').checked ? document.getElementById('SULFI').value : '';
+        let SULFLU = document.getElementById('SULFLU').checked ? document.getElementById('SULFLU').value : '';
+        let VINAC = document.getElementById('VINAC').checked ? document.getElementById('VINAC').value : '';
+        let ZEA = document.getElementById('ZEA').checked ? document.getElementById('ZEA').value : '';
+
         let anotaciones = document.getElementById('anotaciones').value; // observaciones
         let fechaenvio = document.getElementById('fechaenvio').value;
         let enviante = document.getElementById('enviante').value;
         let normal = document.getElementById('normal').value;
         let express = document.getElementById('express').value;
-
         
-        
-
-        generatePDF( NombreSolicitante, Nit, TelefonoContacto, Direccion, Cuidad, PersonaContacto,NombreFacturante,NitFacturante,DireccionFacturante,CuidadFacturante,TelefonoFacturante, fact,matriz,fecha, codigo,multiresiduo,micotoxinaSeleccionada,ditios,antimonio,arsenico,boro,cadmio,cinc,cobre,cromo,estaño,hierro, magnesio,manganeso,mercurio,plata, plomo,selenio,sodio,potasio,metalesSi
-                    ,glyfo,fosetyl,clorato,diquat,oxEtil,acrilamida,amitraz,patulina,gmo,doscuatroD,anotaciones,fechaenvio,enviante, normal,express
-        
-                     
+        generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion,Ciudad,PersonaContacto,NombreFacturante,NitFacturante,CiudadFacturante,TelefonoFacturante, fact,matriz,fecha, codigo,multiresiduo,antimonio,arsenico,boro,cadmio,cinc,cobre,cromo,estaño,hierro, magnesio,manganeso,mercurio,plata, plomo,selenio,sodio,potasio,metalesSi
+                    ,acrilamida,AFL,AFLM,AFLOTA,amitraz,bromuro,ciromazina,clorato,cloratos,cloruro,cloro,DIOX_PCBDs,ditios,ditiosLow,DON,doscuatroD,DTCAN,ETH,ETHOX,FAL,FLON,FLUO,FOLP,fosetyl,FUM,GCCAN,GGA,GMOS,LCCAN,LHR,MC1,MEL,MINO,NEG,NIT,NITRI,OTA,PAH,patulina,PER,PHOS,QU1,QU2,RAD1,S421,SULFA,SULFI,SULFLU,VINAC,ZEA,
+                    anotaciones,fechaenvio,enviante,normal,express
             );
     })
 
+function showInfoPopup(message) {
+  if (document.getElementById('__popup_overlay__')) return;
+  const overlay = document.createElement('div');
+  overlay.id = '__popup_overlay__';
+  Object.assign(overlay.style, {
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0,0,0,0.45)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 9999
+  });
+
+  const box = document.createElement('div');
+  Object.assign(box.style, {
+    background: '#fff',
+    padding: '18px',
+    borderRadius: '10px',
+    maxWidth: '480px',
+    boxShadow: '0 8px 30px rgba(0,0,0,0.25)',
+    textAlign: 'left'
+  });
+
+  const p = document.createElement('p');
+  p.style.margin = '0 0 12px';
+  p.innerHTML = message;
+  box.appendChild(p);
+  const btn = document.createElement('button');
+  btn.textContent = 'Entendido';
+  Object.assign(btn.style, {
+    background: '#4e9af1',
+    color: '#fff',
+    border: 'none',
+    padding: '8px 14px',
+    borderRadius: '8px',
+    cursor: 'pointer'
+  });
+  btn.addEventListener('click', () => document.body.removeChild(overlay));
+  box.appendChild(btn);
+  overlay.appendChild(box);
+  document.body.appendChild(overlay);
+}
+
+const mensajesPopup = {
+  MINO: '⚠️ Para este análisis la muestra debe enviarse en <b>frasco de vidrio</b> y con <b>aluminio en la tapa</b> para evitar contacto con materiales plásticos.',
+  ETHOX: '⚠️ Esta muestra debe enviarse en un <b>empaque herméticamente sellado</b>.',
+  PHOS: '⚠️ Esta muestra debe enviarse en un <b>empaque herméticamente sellado</b>.',
+  SULFI: '⚠️ Esta muestra debe enviarse en un <b>empaque herméticamente sellado</b>.',
+  VINAC: '⚠️ Esta muestra debe enviarse en un <b>empaque herméticamente sellado</b>.',
+  CLTOT: '⚠️ Para este método es necesario enviar la <b>muestra de forma independiente</b>.'
+};
+
+// 
+Object.entries(mensajesPopup).forEach(([id, mensaje]) => {
+  const chk = document.getElementById(id);
+  if (chk) {
+    chk.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        showInfoPopup(mensaje);
+      }
+    });
+  } else {
+    console.warn(`⚠️ No se encontró el checkbox con id="${id}"`);
+  }
+});
 });
 
-async function generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion, Ciudad,PersonaContacto,NombreFacturante,NitFacturante,DireccionFacturante,CuidadFacturante,TelefonoFacturante,fact, matriz,fecha,codigo, multiresiduo,micotoxinaSeleccionada,ditios,antimonio,arsenico,boro,cadmio,cinc,cobre,cromo,estaño,hierro, magnesio,manganeso,mercurio,plata, plomo,selenio,sodio,potasio, metalesSi
-                            ,glyfo,fosetyl,clorato,diquat,oxEtil,acrilamida,amitraz,patulina,gmo,doscuatroD,anotaciones, fechaenvio, enviante,normal,express
-                      
-                          
+async function generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion, Ciudad,PersonaContacto,NombreFacturante,NitFacturante,CiudadFacturante,TelefonoFacturante,fact, matriz,fecha,codigo, multiresiduo,antimonio,arsenico,boro,cadmio,cinc,cobre,cromo,estaño,hierro, magnesio,manganeso,mercurio,plata, plomo,selenio,sodio,potasio, metalesSi
+                            ,acrilamida,AFL,AFLM,AFLOTA,amitraz,bromuro,ciromazina,clorato,cloratos,cloruro,cloro,DIOX_PCBDs,ditios,ditiosLow,DON,doscuatroD,DTCAN,ETH,ETHOX,FAL,FLON,FLUO,FOLP,fosetyl,FUM,GCCAN,GGA,GMOS,LCCAN,LHR,MC1,MEL,MINO,NEG,NIT,NITRI,OTA,PAH,patulina,PER,PHOS,QU1,QU2,RAD1,S421,SULFA,SULFI,SULFLU,VINAC,ZEA,
+                            anotaciones,fechaenvio,enviante,normal,express                          
     ) {
     const image = await loadImage("imagenes/FormatoSolicitud2.jpg");
-    
     const pdf = new jsPDF('1', 'pt', [612, 792]);
 
-    pdf.addImage(image, 'PNG', 0, 0, 612, 792);
-    
+    pdf.addImage(image, 'PNG', 0, 0, 612, 792);   
     pdf.setFontSize(9);
     pdf.text(NombreSolicitante, 130, 155);
     pdf.text(Nit, 448, 155);
@@ -157,28 +235,20 @@ async function generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion, 
     pdf.text(Ciudad, 448, 165);
     pdf.text(PersonaContacto, 130, 175);
     pdf.text(TelefonoContacto, 448, 175);
-    pdf.text(CuidadFacturante, 130,217);
-    pdf.text('- '+DireccionFacturante, 200, 217);
+    pdf.text(CiudadFacturante, 130,217);
     pdf.setFontSize(9);
     pdf.text(NombreFacturante, 130, 205);
     pdf.text(NitFacturante, 448, 205);
     pdf.text(TelefonoFacturante, 448,217);
-    
     pdf.setFontSize(8);
     pdf.text(fact, 130, 227);
-
-    
     pdf.setFontSize(8);
     pdf.text(matriz, 175, 265);
-    //pdf.text(productor, 220, 280);
     pdf.text(fecha, 448, 263);
     pdf.text(codigo, 175, 280);
-    //pdf.text(sampler, 545, 295);
-    //pdf.text(infadicional, 220, 315);
-
 
     // --- LISTADO ORGANIZADO ---
-    let startY = 335;   
+    let startY = 335;   // posición inicial en Y
     let lineHeight = 10; 
 
     pdf.setFontSize(9);
@@ -191,27 +261,9 @@ async function generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion, 
     startY += lineHeight + 7; 
     }
 
-    // --- DITIOS ---
-    if (ditios.trim() !== "") {
-    pdf.text("Ditios:", 60, startY);
-    startY += lineHeight;
-    pdf.text("- " + ditios, 75, startY);
-    startY += lineHeight + 7;
-    }
-
-    // --- MICOTOXINAS ---
-    if (micotoxinaSeleccionada.trim() !== "") {
-    pdf.text("Micotoxinas:", 60, startY);
-    startY += lineHeight;
-    pdf.text("- " + micotoxinaSeleccionada, 75, startY);
-    startY += lineHeight + 7;
-    }
-
     // --- METALES ---
     const metals = [
-    'antimonio', 'arsenico', 'boro', 'cadmio', 'cinc', 'cobre','cromo',
-    'estaño','hierro','magnesio','manganeso','mercurio','plata','plomo',
-    'selenio','sodio','potasio'
+    'antimonio', 'arsenico', 'boro', 'cadmio', 'cinc', 'cobre','cromo','estaño','hierro','magnesio','manganeso','mercurio','plata','plomo','selenio','sodio','potasio'
     ];
 
     const selectedValues = metals
@@ -222,14 +274,12 @@ async function generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion, 
     if (selectedValues.length > 0) {
     pdf.text("Metales:", 60, startY);
     startY += lineHeight;
-    pdf.text("- " + selectedValues.join(", "), 70, startY);
+    pdf.text("                              - " + selectedValues.join(", "), 70, startY);
     startY += lineHeight + 7; 
     }
 
     // --- OTROS ---
-    const otrosIds = [
-    "glyfo","fosetyl","MINO","clorato","diquat",
-    "oxEtil","acrilamida","amitraz","gmo","patulina","doscuatroD"
+    const otrosIds = ["acrilamida","AFL","AFLM","AFLOTA","amitraz","bromuro","ciromazina","clorato","cloratos","cloruro","cloro","DIOX_PCBDs","ditios","ditiosLow","DON","doscuatroD","DTCAN","ETH","ETHOX","FAL","FLON","FLUO","FOLP","fosetyl","FUM","GCCAN","GGA","GMOS","LCCAN","LHR","MC1","MEL","MINO","NEG","NIT","NITRI","OTA","PAH","patulina","PER","PHOS","QU1","QU2","RAD1","S421","SULFA","SULFI","SULFLU","VINAC","ZEA"
     ];
 
     const otrosSeleccionados = otrosIds
@@ -247,27 +297,25 @@ async function generatePDF(NombreSolicitante, Nit, TelefonoContacto, Direccion, 
         startY += lineHeight;
     });
     startY += 7;
-    }
-    
+    }    
 
     const orgSi = document.getElementById('OrgSi');
     const orgNo = document.getElementById('OrgNo');
     if (orgSi.checked) {
-    pdf.text('X', 358, 488); 
+    pdf.text('X', 358, 488); // Coordenadas para el "Sí"
     }
     if (orgNo.checked) {
-    pdf.text('X', 448, 488); 
+    pdf.text('X', 448, 488); // Coordenadas para el "No"
     }
     pdf.setFontSize(10);
     if (document.getElementById("normal").checked) {
-    pdf.text("X", 157, 548);  
+    pdf.text("X", 157, 548);  // Coordenadas para "normal"
     } else if (document.getElementById("express").checked) {
-    pdf.text("X", 292, 548);  
+    pdf.text("X", 292, 548);  // Coordenadas para "express"
     }
  
-
     pdf.setFontSize(8);
-    const maxWidth = 450; 
+    const maxWidth = 450; // ancho máximo
     const lines = pdf.splitTextToSize(anotaciones, maxWidth);  
     pdf.text(lines, 125, 514); 
    
